22b27be58ab4414510571a474c4e6e9fb01dd467
ronitdas <ronitdas.2014@gmail.com>
2019-07-25 17:53:05 -0500

    Update market_agent_rl.cpp

--- market_agent_rl.cpp
+++ market_agent_rl.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#inlude <cstdlib>
 #include <vector>
 
 using namespace std;
@@ -9,10 +10,10 @@ const int stateSpace = 12;
 const double gamma = 0.8;
 const double epsilon = 0.2;
 const double alpha = 0.1;
-const int actionSpace = 10;
-const int episodes = 100;
+const int actionSpace = 3;
+const int episodes = 1000;
 
-enum actions {bid_0, bid_1, bid_2, bid_3, bid_4, bid_5, bid_6, bid_7, bid_8, bid_9};
+//enum actions {bid_0, bid_1, bid_2, bid_3, bid_4, bid_5, bid_6, bid_7, bid_8, bid_9};
 
 int currentState;
 
@@ -20,14 +21,116 @@ vector<int> states {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
 vector<vector<double>> Q(stateSpace, vector<double>(actionSpace, 0));
 
-void takeAction(int biddingAction)
+int generateState(double load, double lmp)
 {
-  double offerPrice;
-  
-  switch (biddingAction)
-    {
-    case 0:
-      offerPrice = 
+	if (load >= 4000 && load <= 4600 && lmp >=1 && lmp <= 6)
+	{
+		return 0;
+	}
+	else if (load >= 4601 && load <= 5000 && lmp >=7 && lmp <= 15)
+	{
+		return 1;
+	}
+	else if (load >= 5001 && load <= 5400 && lmp >=16 && lmp <= 23)
+	{
+		return 2;
+	}
+	else if (load >= 5401 && load <= 5900 && lmp >=24 && lmp <= 30)
+	{
+		return 3;
+	}
+	else if (load >= 5901 && load <= 6300 && lmp >=31 && lmp <= 36)
+	{
+		return 4;
+	}
+	else if (load >= 6301 && load <= 6700 && lmp >=37 && lmp <= 44)
+	{
+		return 5;
+	}
+	else if (load >= 6701 && load <= 7100 && lmp >=45 && lmp <= 52)
+	{
+		return 6;
+	}
+	else if (load >= 7101 && load <= 7400 && lmp >=53 && lmp <= 60)
+	{
+		return 7;
+	}
+	else if (load >= 7401 && load <= 7700 && lmp >=61 && lmp <= 65)
+	{
+		return 8;
+	}
+	else if (load >= 7701 && load <= 8000 && lmp >=66 && lmp <= 72)
+	{
+		return 9;
+	}
+	else if (load >= 8001 && load <= 8300 && lmp >=73 && lmp <= 78)
+	{
+		return 10;
+	}
+	else
+		return 11;
+}
+
+int getState()
+{
+	//Haotian please get the LMP and the Load
+	
+	int currentState = generateState(/*load, lmp */);
+		
+	return currentState;
+}
+	
+
+void updateQ(double reward, int oldState, int newState, int actionIndex)
+{
+	double newQ = (1 - alpha) * Q.at(oldState).at(actionIndex) + alpha * (reward + gamma * newState );
+	
+	Q.at(oldState).at(actionIndex) = newQ;
+}
+	
+	
+
+void takeAction(int currentState, int actionIndex)
+{
+  	double profit;
+	
+	double nextLoad;
+	double nextLMP;
+	
+	int oldState = currentState;
+	int newState;
+	
+	  switch(actionIndex)
+	  {
+		  case 0:
+			  profit = //Haotian generate the MCP and send the value to the Environment;
+		          nextLoad = // ""
+			  nextLMP = // ""
+			  break;
+		  case 1:
+			  profit = //Generate next 10% and send to the simulator
+			  nextLoad = //
+			  nextLMP = //
+			  break;
+		  case 2:
+			  profit  = //Generate next 20%...
+			  nextLoad = //
+			  nextLMP = //
+			  break;
+	  }
+	
+	newState = generateState(nextLoad, nextLMP);
+	
+	updateQ(profit, oldState,newState, actionIndex);
+	
+	
+}
+
+void getRandomAction(int currentState)
+{
+  	int randomActionIndex = rand() % (actionSpace - 0 + 1);
+	takeAction(currentState, randomActionIndex);
+}
   
   
 
@@ -38,11 +141,11 @@ void chooseAction(int currState)
   
   if (rndVal < epsilon)
     {
-      getRandomAction();
+      getRandomAction(currState);
     }
   else
     {
-      int maxQValIndex = INT_MIN;
+      int maxQValIndex = 0;
       
       for (int i = 0; i < actionSpace; i++)
 	{
@@ -52,7 +155,8 @@ void chooseAction(int currState)
 	    }
 	}
       
-      takeAction()
+      takeAction(currState, maxQValIndex);
+  }
       
       
       
@@ -86,7 +190,7 @@ int main()
     {
       for (int j = 0; j < stateSpace; j++)
 	{
-	  startEpisode(states.at(j));
+	  startEpisode(getState());
 	}
     }
   
